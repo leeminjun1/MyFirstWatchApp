2173aa535ef3d67b0af10cbf33cb9f6474a1b00b
leeminjun1 <lmjdooda@gmail.com>
2026-07-15 08:19:29 +0900

    Initial commit for MyFirstWatchApp

---
+++ MyFirstWatchApp Watch App/MyFirstWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstWatchAppApp.swift
+//  MyFirstWatchApp Watch App
+//
+//  Created by 이민준 on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
